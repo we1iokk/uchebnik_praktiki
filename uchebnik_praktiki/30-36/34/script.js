20bb278087d5a3445107107506ae12fc338df738
we1iokk <shagapovaar16@gmail.com>
2023-12-21 13:23:47 +0300

    Add files via upload

---
+++ uchebnik_praktiki/30-36/34/script.js
@@ -0,0 +1,18 @@
+"use strict"
+
+let button = document.querySelector('#button');
+
+button.addEventListener('click', function() {
+    let inputs = document.querySelectorAll('#test input');
+    for (let input of inputs) {
+        if (input.checked) {
+            if (input.hasAttribute('data-right')) {
+                input.classList.remove('wrong');
+                input.classList.add('right');
+            } else {
+                input.classList.remove('right');
+                input.classList.add('wrong');
+            }
+        }
+    }
+});
